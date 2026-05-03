948e15c07df3694e8e4661da72ca36cc7c63e7fe
PrinceLemayian <189862853+PrinceLemayian@users.noreply.github.com>
2026-05-03 15:41:31 +0300

    implement functionality to delete account

--- script.js
+++ script.js
@@ -168,3 +168,24 @@ btnTransfer.addEventListener('click', function (e) {
     updateUI(currentAccount);
   }
 });
+
+btnClose.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  if (
+    inputCloseUsername.value === currentAccount.username &&
+    Number(inputClosePin.value) === currentAccount.pin
+  ) {
+    const index = accounts.findIndex(
+      acc => acc.username === currentAccount.username,
+    );
+
+    inputCloseUsername.value = inputClosePin.value = '';
+
+    // Delete Account
+    accounts.splice(index, 1);
+
+    // Hide UI
+    containerApp.style.opacity = 0;
+  }
+});
